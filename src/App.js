dcd2a21c3f045282fb3aedcc3f1f71c00d2d8e8b
Indira Djambaeva <indira.djambaeva.kk@gmail.com>
2022-04-14 01:53:54 +0600

    First react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import './app.scss';
+
+// function App() {
+//   return (
+//     <div className="app">
+//       <h1>Hello React</h1>
+//       <h2>Hello again</h2>
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import Navbar from "./components/Navbar/Navbar";
+import "./app.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar />
     </div>
   );
 }
